fa2a852ccc23f5fac000e9a5256f6692667606db
unknown <TDiefenbach@.(none)>
2009-11-20 19:28:26 -0800

    hover enemy

---
+++ Cyclyc/JetpackGirl/HoverEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using Cyclyc.Framework;
+
+namespace Cyclyc.JetpackGirl
+{
+    class HoverEnemy : JetpackEnemy
+    {
+        public HoverEnemy(Game1 game, EnemyPool p)
+            : base(game, p)
+        {
+            jetpack.Gravity = 0.0f;
+        }
+        public override bool ShouldJet
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public override bool ShouldJump
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
--- Cyclyc/JetpackGirl/JetGame.cs
+++ Cyclyc/JetpackGirl/JetGame.cs
@@ -23,6 +23,7 @@ namespace Cyclyc.JetpackGirl
         //and maybe jetpack?
         RobotEnemyPool robots;
         SpiderEnemyPool spiders;
+        HoverEnemyPool hovers;
 
         public JetGame(Game1 game)
             : base(game)
@@ -30,6 +31,7 @@ namespace Cyclyc.JetpackGirl
             SongName = "jet";
             robots = new RobotEnemyPool(this);
             spiders = new SpiderEnemyPool(this);
+            hovers = new HoverEnemyPool(this);
         }
 
         protected override void AddBackground(string n, float spd)
@@ -61,19 +63,25 @@ namespace Cyclyc.JetpackGirl
 
         public override EnemyMaker MakeEnemy(bool leftToRight, int difficulty)
         {
+            double r = rgen.NextDouble();
             return (c) =>
                 {
                     JetpackEnemy en;
                     //scale up or down based on difficulty, etc
                     float sizeMultiplier = (float)(rgen.NextDouble() * 1.0 + 0.5);
-                    if (rgen.NextDouble() < 0.5)
+                    if (r < 0.3)
                     {
                         en = robots.Create(c, "robot", 2, leftToRight, 0, (int)(16 * sizeMultiplier), (int)(21 * sizeMultiplier), (float)((rgen.NextDouble() * 1.0) + 0.25), (int)(3 * sizeMultiplier), (int)(3 * sizeMultiplier), (int)(10 * sizeMultiplier), (int)(18 * sizeMultiplier), difficulty);
                     }
-                    else
+                    else if (r < 0.6)
                     {
                         en = spiders.Create(c, "spider", 3, leftToRight, (int)(View.Height / 2 - 34 * sizeMultiplier), (int)(sizeMultiplier * 102 / 3), (int)(sizeMultiplier * 17), (float)(rgen.NextDouble() * 1.0) + 0.25f, (int)(sizeMultiplier * 5), (int)(sizeMultiplier * 4), (int)(sizeMultiplier * ((102 / 3) - 10)), (int)(sizeMultiplier * 11), difficulty);
                     }
+                    else
+                    {
+                        en = hovers.Create(c, "hover", 3, leftToRight, (int)(rgen.NextDouble()*100 + 10), (int)(16 * sizeMultiplier), (int)(21 * sizeMultiplier), (float)((rgen.NextDouble() * 1.0) + 0.25), (int)(3 * sizeMultiplier), (int)(3 * sizeMultiplier), (int)(10 * sizeMultiplier), (int)(18 * sizeMultiplier), difficulty);
+                    }
+
                     en.Target = jg;
                 };
         }
@@ -100,7 +108,7 @@ namespace Cyclyc.JetpackGirl
                 do
                 {
                     jg.Position = new Vector2((float)(rgen.NextDouble() * 170)+30, 0);
-                } while (spiders.Collide(jg).Count != 0 || robots.Collide(jg).Count != 0);
+                } while (spiders.Collide(jg).Count != 0 || robots.Collide(jg).Count != 0 || hovers.Collide(jg).Count != 0);
                 Vector2 ret = jg.Position;
                 jg.Position = oldPos;
                 return ret;
@@ -117,11 +125,13 @@ namespace Cyclyc.JetpackGirl
         {
             robots.Update(gameTime);
             spiders.Update(gameTime);
+            hovers.Update(gameTime);
             base.Update(gameTime);
             if (jg.Attacking)
             {
                 List<CycEnemy> hitRobots = robots.Collide(jg.Wrench);
                 List<CycEnemy> hitSpiders = spiders.Collide(jg.Wrench);
+                List<CycEnemy> hitHovers = hovers.Collide(jg.Wrench);
                 foreach (CycEnemy hit in hitRobots)
                 {
                     ((JetpackEnemy)(hit)).Hit(jg.Position.X);
@@ -130,9 +140,14 @@ namespace Cyclyc.JetpackGirl
                 {
                     ((JetpackEnemy)(hit)).Hit(jg.Position.X);
                 }
+                foreach (CycEnemy hit in hitHovers)
+                {
+                    ((JetpackEnemy)(hit)).Hit(jg.Position.X);
+                }
             }
             List<CycEnemy> killerRobots = robots.Collide(jg);
             List<CycEnemy> killerSpiders = spiders.Collide(jg);
+            List<CycEnemy> killerHovers = hovers.Collide(jg);
             foreach (CycEnemy en in killerRobots)
             {
                 if (!((JetpackEnemy)en).IsHit && !jg.Dying)
@@ -141,6 +156,13 @@ namespace Cyclyc.JetpackGirl
                 }
             }
             foreach (CycEnemy en in killerSpiders)
+            {
+                if (!((JetpackEnemy)en).IsHit && !jg.Dying)
+                {
+                    KillPlayer();
+                }
+            }
+            foreach (CycEnemy en in killerHovers)
             {
                 if (!((JetpackEnemy)en).IsHit && !jg.Dying)
                 {
@@ -163,6 +185,14 @@ namespace Cyclyc.JetpackGirl
                     en.Die();
                 }
             }
+            foreach (CycEnemy en in hovers.Enemies)
+            {
+                if (en.Alive && ((JetpackEnemy)en).KnockedOffScreen)
+                {
+                    NextGame.DeliverEnemy(en.Position.X < 0 ? true : false, en.Difficulty);
+                    en.Die();
+                }
+            }
             if (jg.Dying)
             {
                 jg.StartPosition = StartPosition;
@@ -174,6 +204,7 @@ namespace Cyclyc.JetpackGirl
             base.DrawInnards(gt);
             robots.Draw(gt);
             spiders.Draw(gt);
+            hovers.Draw(gt);
         }
     }
 }
